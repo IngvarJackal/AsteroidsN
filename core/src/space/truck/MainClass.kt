a05bc2f06302c40e93950771cf9981650dadc102
ingvar <rz703a@gmail.com>
2016-02-17 10:03:32 +0200

    added simple physical model

--- core/src/space/truck/MainClass.kt
+++ core/src/space/truck/MainClass.kt
@@ -5,21 +5,58 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.World
+import space.truck.entities.CelestialBody
+import space.truck.entities.SpaceObject
 
 class MainClass : ApplicationAdapter() {
     internal var batch: SpriteBatch? = null
-    internal var img: Texture? = null
+    internal var world: World? = null
+    internal var earth: CelestialBody? = null
+    internal var moon: SpaceObject? = null;
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        world = World(Vector2(0f, 0f), true);
+
+        earth = CelestialBody(Texture("badlogic-small.jpg"), world!!)
+        earth!!.setInCenter()
+
+        moon = SpaceObject(Texture("badlogic-very-small.jpg"), world!!)
+        moon!!.setPosition(0f, 0f);
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 1f, 0f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch?.begin()
-        batch?.draw(img, 50f, 50f)
-        batch?.end()
+        world!!.step(Gdx.graphics.getDeltaTime(), 6, 2)
+
+        moveObject(moon!!, arrayOf(earth!!))
+        moon!!.updatePositionPhys()
+
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch!!.begin();
+
+        earth!!.draw(batch!!)
+        moon!!.draw(batch!!)
+
+        batch!!.end();
+    }
+
+    fun moveObject(obj: SpaceObject, bodies: Array<CelestialBody>) {
+        for (body in bodies) {
+            var distance = Vector2.dst(obj.body.position.x, obj.body.position.y,
+                                       body.body.position.x, body.body.position.y)
+            var force = Math.max(Math.min(((obj.body.mass * body.mass) /
+                         (Math.pow(distance.toDouble(), 2.0))).toFloat(), 100f), 1e-3f)
+            System.out.println("Object: ${obj.body.position}, body: ${body.body.position}")
+            System.out.println("Distance: ${distance}, force: ${force}")
+            System.out.println("FINAL: ${(body.body.position.x - obj.body.position.x)*force}, ${(body.body.position.y - obj.body.position.y)*force}")
+            obj.body.applyForce((body.body.position.x - obj.body.position.x)*force,
+                                (body.body.position.y - obj.body.position.y)*force,
+                                obj.body.position.x,
+                                obj.body.position.y,
+                                true)
+        }
     }
 }
--- desktop/src/space/truck/desktop/DesktopLauncher.java
+++ desktop/src/space/truck/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import space.truck.MainClass;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 720;
+        config.width = 720;
 		new LwjglApplication(new MainClass(), config);
 	}
 }
